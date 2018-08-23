54b5ad0bf93b9e0845865e8ab538a63c3fd819f0
洛忆昔 <mrwang90hou@126.com>
2018-08-23 21:55:41 +0800

    远程连接修改

--- MRVLCPlayer/RemoteViewController.m
+++ MRVLCPlayer/RemoteViewController.m
@@ -9,6 +9,9 @@
 #import "RemoteViewController.h"
 #import "MRVLCPlayer.h"
 
+//模糊视频
+#define NOTHIGHDEFINITION @"rtsp://192.72.1.1/liveRTSP/av1"
+#define URL @"http://192.72.1.1/SD/Normal/NK_D20180821_173641_1440.MP4"
 @implementation RemoteViewController
 
 - (void)viewDidAppear:(BOOL)animated{
@@ -27,10 +30,10 @@
 - (IBAction)remotePlay:(id)sender {
 
     MRVLCPlayer *player = [[MRVLCPlayer alloc] init];
-    
     player.bounds = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.width / 16 * 9);
     player.center = self.view.center;
-    player.mediaURL = [NSURL URLWithString:@"http://202.198.176.113/video002/2015/mlrs.rmvb"];
+//    player.mediaURL = [NSURL URLWithString:@"http://baobab.kaiyanapp.com/api/v1/playUrl?vid=39183&editionType=normal&source=qcloud"];
+    player.mediaURL = [NSURL URLWithString:NOTHIGHDEFINITION];
     
     [player showInView:self.view.window];
 }
